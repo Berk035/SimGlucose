40f897317f394128f8188f8f657a2a2b2a283e42
Berk <berkagin.5@gmail.com>
2022-08-02 08:23:11 +0300

    traning exp

--- examples/apply_customized_controller.py
+++ examples/apply_customized_controller.py
@@ -26,6 +26,7 @@ class MyController(Controller):
                  controller action contains two entries: basal, bolus
         '''
         self.state = observation
+
         action = Action(basal=0, bolus=0)
         return action
 
--- examples/run_user_interface.py
+++ examples/run_user_interface.py
@@ -1,19 +1,16 @@
 from simglucose.simulation.user_interface import simulate
-import unittest
-from unittest.mock import patch
-import shutil
-import os, inspect
+import keyboard as event
 
-parentdir = os.path.join(os.path.expanduser("~"),'PycharmProjects','simglucose')
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-output_folder = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                                           '..', '..', 'examples', 'results'))
-print(output_folder)
-
-# animation, parallel, save_path, sim_time, scenario, scenario random
-# seed, start_time, patients, sensor, sensor seed, insulin pump,
-# controller
-# mock_input=patch('builtins.input')
-# mock_input.side_effect = ['y', 'n', output_folder, '24', '1', '2','6', '5', '1', 'd', '1', '1', '2', '1']
+# def main():
+#     while True:
+#         try:  # used try so that if user pressed other than the given key error will not be shown
+#             if event.is_pressed('q'):  # if key 'q' is pressed
+#                 print('You Pressed A Key!')
+#                 break  # finishing the loop
+#         except:
+#             continue  # if user pressed a key other than the given key the loop will break
+#
+# if __name__ == '__main__':
+#     main()
 
 simulate()
